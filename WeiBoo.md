e68c7bddf289d4547e2b5b16cce5acde3f57ac9b
qianerye <1262519863@qq.com>
2020-01-07 20:11:11 +0800

    wwm-1-7pm

---
+++ WeiBoo.md
@@ -0,0 +1,16 @@
+## WeiBoo开发文档
+
+
+
+### 组件
+
+- bar-navigation
+
+  ```javascript
+  <bar-navigation type="home" barText='发微博'></bar-navigation>
+  ```
+
+  > type : 可不传  可选( home , search  ) 
+
+  >  barText : 可不传  ,  传入则中间会显示文本内容   String
+
--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 //app.js
 App({
-  
+  globalData :{
+
+  },
+  onLaunch: function () {
+    let menuButtonObject = wx.getMenuButtonBoundingClientRect();
+    wx.getSystemInfo({
+      success: res => {
+        let statusBarHeight = res.statusBarHeight,
+          navTop = menuButtonObject.top,//胶囊按钮与顶部的距离
+          navHeight = statusBarHeight + menuButtonObject.height + (menuButtonObject.top - statusBarHeight)*2;//导航高度
+        this.globalData.navHeight = navHeight;
+        this.globalData.navTop = navTop;
+        this.globalData.windowHeight = res.windowHeight;
+      },
+      fail(err) {
+        console.log(err);
+      }
+    })
+  }
 })
