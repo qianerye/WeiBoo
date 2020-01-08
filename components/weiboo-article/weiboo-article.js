49c752fe11b217bf9a319f3a8e59e46ecc1b07f2
qianerye <1262519863@qq.com>
2020-01-08 22:55:00 +0800

    wwm

--- components/weiboo-article/weiboo-article.js
+++ components/weiboo-article/weiboo-article.js
@@ -1,23 +1,13 @@
 // components/weiboo-article/weiboo-article.js
 Component({
-  /**
-   * 组件的属性列表
-   */
+
   properties: {
-
+    card : Object,
+    type:String
   },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  lifetimes :{
+    attached : function (){
+      console.log(this.data.card)
+    }
   }
 })
