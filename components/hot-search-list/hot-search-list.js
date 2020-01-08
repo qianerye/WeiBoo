b48068ac946501b6a2f65d480a59e7979f820db1
kongmengzhen <993686326@qq.com>
2020-01-08 22:48:12 +0800

    hotSearch

--- components/hot-search-list/hot-search-list.js
+++ components/hot-search-list/hot-search-list.js
@@ -4,23 +4,44 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    
   },
+ 
 
   /**
    * 组件的初始数据
    */
   data: {
-    selectHeight:0
+    selectHeight:0,
+    searchData: {}
   },
  
-  lifetimes:{
+  lifetimes:{    
     attached: function () { 
-      let ht=wx.getSystemInfoSync()
+    let that = this
+    wx.request({
+      url:'https://api.weibo.cn/2/page?gsid=_2A_p8JoUDCoX0kygqlQXYSVII2sPlPcwqaaPJzmFeEtLG8oFeQ7UvWFSEYRw0nE_47jKzB0qp2abTIjLVPiMyniOd&uid=5959130517&from=1885396040&wm=90163_90001&c=weixinminiprogram&s=92caf721&lang=zh_CN&containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&count=20',
+      header: {
+        'content-type': 'application/json' 
+      },
+      success(res) {
+        // console.log(res.data)
+        that.setData({
+          searchData: res.data.cards[0]
+        })
+        console.log(that.data.searchData);
+      }
+    
+    },
+    
+    )
+    
+    let ht=wx.getSystemInfoSync()
     console.log(ht.windowHeight)
     this.setData({
       selectHeight:ht.windowHeight-60
-    })
+    })   
+
     }
   },
 
