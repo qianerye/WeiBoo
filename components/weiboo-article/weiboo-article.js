accff81844b9d38211cb9fd6d724346b13905269
qianerye <1262519863@qq.com>
2020-01-09 09:55:10 +0800

    2020-1-9

--- components/weiboo-article/weiboo-article.js
+++ components/weiboo-article/weiboo-article.js
@@ -7,7 +7,22 @@ Component({
   },
   lifetimes :{
     attached : function (){
-      console.log(this.data.card)
+      let _card = this.data.card
+      // _card.created_at = new Date(_card.created_at).getDay()
+      // console.log(JSON.stringify(this.data.card))
+      
+    }
+  },
+  methods :{
+    handleDetailClick(e){
+      wx.setStorageSync(
+        "card" , e.currentTarget.dataset.card
+      )
+      wx.navigateTo({
+        url: "/pages/detail/detail"
+      })
+      
     }
   }
+
 })
