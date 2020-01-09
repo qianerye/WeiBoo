a8cd62ab1fe97e4c32fb81c989b927447839770f
BlueClodpate <360354795@qq.com>
2020-01-09 21:41:31 +0800

    热门 WWM

--- components/weiboo-article/weiboo-article.js
+++ components/weiboo-article/weiboo-article.js
@@ -2,26 +2,47 @@
 Component({
 
   properties: {
-    card : Object,
-    type:String
+    card: Object,
+    type: String
   },
-  lifetimes :{
-    attached : function (){
+  lifetimes: {
+    attached: function () {
       let _card = this.data.card
-      // _card.created_at = new Date(_card.created_at).getDay()
-      // console.log(JSON.stringify(this.data.card))
-      
+      let month = new Date(_card.created_at).getMonth() + 1
+      let day = new Date(_card.created_at).getDay()
+      let hour = new Date(_card.created_at).getHours()
+      let min = new Date(_card.created_at).getMinutes()
+      month = month >= 10 ? month : "0" + month
+      day = day >= 10 ? day : "0" + day
+      hour = hour >= 10 ? hour : "0" + hour
+      min = min >= 10 ? min : "0" + min
+      let created_at = month + '-' + day + " " + hour + ':' + min
+      _card.created_at = created_at
+      let reg = /<[^<>]+>/g
+      _card.source = _card.source.replace(reg, '')
+      if (_card.pic_num > 0) {
+        let keys = Object.keys(_card.pic_infos)
+        let images = keys.reduce((imgs , item) => {
+          imgs.push(_card.pic_infos[item])
+          return imgs
+        } , [])
+        _card.images = images
+        console.log(images)
+      }
+      this.setData({
+        card: _card
+      })
     }
   },
-  methods :{
-    handleDetailClick(e){
+  methods: {
+    handleDetailClick(e) {
       wx.setStorageSync(
-        "card" , e.currentTarget.dataset.card
+        "card", e.currentTarget.dataset.card
       )
       wx.navigateTo({
         url: "/pages/detail/detail"
       })
-      
+
     }
   }
 
--- pages/find/find.js
+++ pages/find/find.js
@@ -15,9 +15,198 @@ Page({
     cards: []
   },
   handleItemClick(e){    
+    let index = e.target.dataset.index
     let ele = 'ele' + e.target.dataset.index
     this.setData({
-      navIndex: e.currentTarget.dataset['index']
+      navIndex: index
+    })
+    this.getRect("#"+ele)
+    switch (index) {
+      case 0:
+        wx.request({
+          url: "https://api.weibo.cn/2/statuses/unread_hot_timeline?new_version=0&gsid=_2A_p8JoUDCoX0kygqlQXYSVII2sPlPcwqaaPJzmFeEtLG8oFeQ7UvWFSEYRw0nE_47jKzB0qp2abTIjLVPiMyniOd&uid=5959130517&from=1885396040&wm=90163_90001&c=weixinminiprogram&networktype=wifi&v_p=60&s=92caf721&lang=zh_CN",
+          method: "POST",
+          header: {
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          data: {
+            group_id: 102803,
+            extparam: 'discover|new_feed',
+            count: 25,
+            containerid: 102803,
+            trim_level: 1,
+            trim_page_recom: 0,
+            need_jump_scheme: 1,
+            preAdInterval: -1,
+            since_id: 0,
+            fid: 102803,
+            lfid: '102803_ctg1_4688_-_ctg1_4688',
+            refresh_type: 'auto'
+          },
+          success: result => {
+            this.setData({
+              cards: result.data.statuses,
+              ad: result.data.ad
+            })
+          },
+          fail: error => {
+            console.log(error.message)
+          }
+        })
+        break;
+      case 2:
+        wx.request({
+          url: 'https://api.weibo.cn/2/cardlist?gsid=_2A_p_i4vZCoV0kysilgrYS1oO2cDlPcwqBqQTzmFeEtLG4IFeQ5JQWFSEbotX0iLAZIFlue5-f6UoAhMyMyUfXDlc&from=1885396040&c=weixinminiprogram&s=e3c1c084&containerid=231643_1&page=1',
+          success: result => {
+            console.log(result)
+          },
+          fail: error => {
+            console.log(error.message)
+          }
+        })
+        break;
+      case 3:
+        wx.request({
+          url: "https://api.weibo.cn/2/statuses/unread_hot_timeline?new_version=0&gsid=_2A_p8JoUDCoX0kygqlQXYSVII2sPlPcwqaaPJzmFeEtLG8oFeQ7UvWFSEYRw0nE_47jKzB0qp2abTIjLVPiMyniOd&uid=5959130517&from=1885396040&wm=90163_90001&c=weixinminiprogram&networktype=wifi&v_p=60&s=92caf721&lang=zh_CN",
+          method: "POST",
+          header: {
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          data: {
+            group_id: 1028034288,
+            extparam: 'discover|new_feed',
+            count: 25,
+            containerid: '102803_ctg1_4288_-_ctg1_4288',
+            trim_level: 1,
+            trim_page_recom: 0,
+            need_jump_scheme: 1,
+            preAdInterval: -1,
+            since_id: 0,
+            fid: '102803_ctg1_4288_-_ctg1_4288',
+            lfid: 'video',
+            refresh_type: 'auto'
+          },
+          success: result => {
+            this.setData({
+              cards: result.data.statuses
+            })
+          },
+          fail: error => {
+            console.log(error.message)
+          }
+        })
+        break;
+      case 4:
+        wx.request({
+          url: "https://api.weibo.cn/2/statuses/unread_hot_timeline?new_version=0&gsid=_2A_p8JoUDCoX0kygqlQXYSVII2sPlPcwqaaPJzmFeEtLG8oFeQ7UvWFSEYRw0nE_47jKzB0qp2abTIjLVPiMyniOd&uid=5959130517&from=1885396040&wm=90163_90001&c=weixinminiprogram&networktype=wifi&v_p=60&s=92caf721&lang=zh_CN",
+          method: "POST",
+          header: {
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          data: {
+            group_id: 1028034488,
+            extparam: 'discover|new_feed',
+            count: 25,
+            containerid: '102803_ctg1_4488_-_ctg1_4488',
+            trim_level: 1,
+            trim_page_recom: 0,
+            need_jump_scheme: 1,
+            preAdInterval: -1,
+            since_id: 0,
+            fid: '102803_ctg1_4488_-_ctg1_4488',
+            lfid: '102803_ctg1_4288_-_ctg1_4288',
+            refresh_type: 'auto'
+          },
+          success: result => {
+            this.setData({
+              cards: result.data.statuses
+            })
+          },
+          fail: error => {
+            console.log(error.message)
+          }
+        })
+        break;
+      case 5:
+        wx.request({
+          url: "https://api.weibo.cn/2/statuses/unread_hot_timeline?new_version=0&gsid=_2A_p8JoUDCoX0kygqlQXYSVII2sPlPcwqaaPJzmFeEtLG8oFeQ7UvWFSEYRw0nE_47jKzB0qp2abTIjLVPiMyniOd&uid=5959130517&from=1885396040&wm=90163_90001&c=weixinminiprogram&networktype=wifi&v_p=60&s=92caf721&lang=zh_CN",
+          method: "POST",
+          header: {
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          data: {
+            group_id: 1028034388,
+            extparam: 'discover|new_feed',
+            count: 25,
+            containerid: '102803_ctg1_4388_-_ctg1_4388',
+            trim_level: 1,
+            trim_page_recom: 0,
+            need_jump_scheme: 1,
+            preAdInterval: -1,
+            since_id: 0,
+            fid: '102803_ctg1_4388_-_ctg1_4388',
+            lfid: '102803_ctg1_4688_-_ctg1_4688',
+            refresh_type: 'auto'
+          },
+          success: result => {
+            this.setData({
+              cards: result.data.statuses
+            })
+          },
+          fail: error => {
+            console.log(error.message)
+          }
+        })
+        break;
+      case 6:
+        wx.request({
+          url: "https://api.weibo.cn/2/statuses/unread_hot_timeline?new_version=0&gsid=_2A_p8JoUDCoX0kygqlQXYSVII2sPlPcwqaaPJzmFeEtLG8oFeQ7UvWFSEYRw0nE_47jKzB0qp2abTIjLVPiMyniOd&uid=5959130517&from=1885396040&wm=90163_90001&c=weixinminiprogram&networktype=wifi&v_p=60&s=92caf721&lang=zh_CN",
+          method: "POST",
+          header: {
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          data: {
+            group_id: 1028034688,
+            extparam: 'discover|new_feed',
+            count: 25,
+            containerid: '102803_ctg1_4688_-_ctg1_4688',
+            trim_level: 1,
+            trim_page_recom: 0,
+            need_jump_scheme: 1,
+            preAdInterval: -1,
+            since_id: 0,
+            fid: '102803_ctg1_4688_-_ctg1_4688',
+            lfid: '102803_ctg1_4388_-_ctg1_4388',
+            refresh_type: 'auto'
+          },
+          success: result => {
+            this.setData({
+              cards: result.data.statuses
+            })
+          },
+          fail: error => {
+            console.log(error.message)
+          }
+        })
+        break;
+
+    }
+  },
+  getRect(ele){
+    let that=this;
+    wx.createSelectorQuery().select(ele).boundingClientRect(function(rect){     
+      let moveParams=that.data.moveParams
+      moveParams.subLeft = rect.left;
+      moveParams.subHalfWidth = rect.width / 2;     
+      that.moveTo()
+    }).exec()   
+  },
+  moveTo() {
+    let { subLeft, screenHalfWidth, subHalfWidth, scrollLeft} = this.data.moveParams
+    let distance = subLeft - screenHalfWidth + subHalfWidth;
+    scrollLeft = scrollLeft + distance;
+    this.setData({
+      scrollLeft: scrollLeft
     })
     this.getRect("#"+ele)
   },
@@ -47,7 +236,6 @@ Page({
     })
   },  
   onLoad(){
-    console.log(App)
     this.setData({
       contentHeight: App.globalData.windowHeight - 112
     })
@@ -73,8 +261,8 @@ Page({
       },
       success: result => {
         this.setData({
-          cards : result.data.statuses,
-          ad : result.data.ad
+          cards: result.data.statuses,
+          ad: result.data.ad
         })
       },
       file: error => {
