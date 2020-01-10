8538b859dce70c593a638d8a3d3e7faf8f4b4a94
BlueClodpate <360354795@qq.com>
2020-01-10 10:23:07 +0800

    详情页推荐

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -3,7 +3,8 @@ Page({
 	data: {
 		cardInfo: null,
 		cardCommnent: null,
-		cardTrend: null
+		cardTrend: null,
+		recommendUser: null
 	},
 
 	onLoad: function (options) {
@@ -16,16 +17,20 @@ Page({
 				this.setData({
 					cardInfo: card,
 					cardCommnent: result.data
-				})
+				});
 			}
 		});
 		wx.request({
 			url: `https://api.weibo.cn/2/statuses/extend?gsid=_2A_p8JoUDCoX0kygqlQXYSVII2sPlPcwqaaPJzmFeEtLG8oFeQ7UvWFSEYRw0nE_47jKzB0qp2abTIjLVPiMyniOd&uid=5959130517&wm=90163_90001&from=1885396040&c=weixinminiprogram&s=92caf721&id=4456673508669036&mid=${card.id}&recommend_scene=136,137,141,152&count=20&lfid=102803_ctg1_4488_-_ctg1_4488`,
 			method: 'GET',
 			success: result => {
-				this.setData({
-					cardTrend: result.data.trend.feeds
-				})
+				if(result.data.trend && result.data.trend.feeds) {
+					this.setData({
+						cardTrend: result.data.trend.feeds,
+						recommendUser: result.data.wechat_recommend['141'].users
+					});
+					// console.log(result, this.data)
+				}
 			}
 		})
 	}
